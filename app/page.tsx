20944837cc862d881fafb882e14fe3b5afb3d1e0
Sahak Arzoumanian <arzsahag@gmail.com>
2023-08-11 12:00:32 -0700

    attempting to fix vh

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <About />
       <Gallery />
