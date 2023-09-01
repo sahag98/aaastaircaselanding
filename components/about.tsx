79bc30f79d2a85b501eb25a52e0364fb4f43c881
Sahak Arzoumanian <arzsahag@gmail.com>
2023-08-31 17:55:32 -0700

    apos escape

--- components/about.tsx
+++ components/about.tsx
@@ -15,12 +15,12 @@ const About = () => {
       </h2>
       <div className="flex flex-col shadow-xl lg:w-1/2 z-30 bg-[#ffffff] rounded-md p-5 lg:gap-0 md:gap-0 gap-5 items-center justify-center">
         <p className="tracking-wide text-center lg:leading-8 leading-7 justify-center items-center self-center align-middle">
-          We are a custom manufacturer of modern staircases and railings and
-          we're born out of a dream to hire quality people to create quality
-          products to simplify the modern railing market. We engineer, design,
-          manufacturer, pack and ship every item in our own facilities and
-          customized designs are also welcomed. We have our own design team to
-          help customers develop their ideas into drawings.
+          We&apos;re a custom manufacturer of modern staircases and railings and
+          we&apos;re born out of a dream to hire quality people to create
+          quality products to simplify the modern railing market. We engineer,
+          design, manufacturer, pack and ship every item in our own facilities
+          and customized designs are also welcomed. We have our own design team
+          to help customers develop their ideas into drawings.
         </p>
       </div>
     </div>
